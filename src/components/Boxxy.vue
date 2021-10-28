92449df299188a1183f6f5c83c8845bd6c018a5c
Blessing Alfred <blealf@gmail.com>
2021-10-28 22:39:59 +0100

    added inset and change UX

--- src/components/Boxxy.vue
+++ src/components/Boxxy.vue
@@ -1,27 +1,5 @@
 <template>
   <div class="boxxy">
-    <button
-      class="show-color-picker"
-      @click="showColorPicker = !showColorPicker"
-    >
-      {{ showColorPicker ? 'Apply' : 'Change color' }}
-    </button>
-    <div
-      v-if="showColorPicker"
-      :style="{ background: color }"
-      class="color-picker"
-    >
-      <ColorPicker
-        theme="light"
-        :color="color"
-        :sucker-hide="showColorPicker"
-        :sucker-canvas="suckerCanvas"
-        :sucker-area="suckerArea"
-        @changeColor="changeColor"
-        @openSucker="openSucker"
-      />
-    </div>
-
     <!-- Sliders -->
     <div class="sliders">
       <!-- Horizontal -->
@@ -93,6 +71,42 @@
           '--slider-tooltip-bg': `${boxColor}`,
         }"
       ></Slider>
+      <!-- Change Color -->
+      <h3>Shadow color: {{ color }}</h3>
+      <button
+        class="show-color-picker"
+        @click="showColorPicker = !showColorPicker"
+      >
+        {{ showColorPicker ? 'Apply' : 'Change color' }}
+      </button>
+      <div
+        v-if="showColorPicker"
+        :style="{ background: color }"
+        class="color-picker"
+        @blur="showColorPicker = !showColorPicker"
+      >
+        <ColorPicker
+          theme="light"
+          :color="color"
+          :sucker-hide="showColorPicker"
+          :sucker-canvas="suckerCanvas"
+          :sucker-area="suckerArea"
+          @changeColor="changeColor"
+          @openSucker="openSucker"
+        />
+      </div>
+      <!-- Change Color -->
+      <h3>Inset: {{ inset ? 'inset' : '' }}</h3>
+      <button
+        class="show-color-picker"
+        @click="inset = !inset"
+        :style="{
+          background: inset ? boxColor : '#c63b3b',
+          color: inset ? '#000' : '#fff',
+        }"
+      >
+        {{ inset ? 'INSET ON' : 'INSET OFF' }}
+      </button>
     </div>
 
     <!-- The Box -->
@@ -100,12 +114,17 @@
       class="box-shadow"
       :style="{
         'background-color': `${boxColor}`,
-        'box-shadow': `${horizontal}px ${vertical}px ${blur}px ${spread}px ${color}`,
+        'box-shadow': `${horizontal}px ${vertical}px ${blur}px ${spread}px ${color} ${
+          inset ? 'inset' : ''
+        }`,
       }"
     >
-      {{ `box-shadow: ${horizontal}px ${vertical}px ${5}px ${0}px ${color}` }}
+      {{
+        `box-shadow: ${horizontal}px ${vertical}px ${5}px ${0}px ${color} ${
+          inset ? inset : ''
+        }`
+      }}
     </div>
-    {{ color }}
   </div>
 </template>
 
@@ -127,10 +146,10 @@ export default {
       vertical: 10,
       blur: 10,
       spread: 0,
-      min: -190,
-      max: 190,
       opacity: 0.5,
       inset: false,
+      min: -190,
+      max: 190,
       color: 'rgba(34, 31, 26, 0.8)',
       boxColor: 'rgba(59, 198, 198, 0.8)',
       showColorPicker: false,
@@ -152,6 +171,12 @@ export default {
     },
   },
   methods: {
+    slideMin(name) {
+      return name === 'opacity' ? 0.05 : this.min;
+    },
+    slideMax() {
+      return name === 'opacity' ? 0.99 : this.max;
+    },
     changeColor(color) {
       const { r, g, b, a } = color.rgba;
       this.color = `rgba(${r}, ${g}, ${b}, ${a})`;
@@ -176,34 +201,42 @@ export default {
   width: 100%;
   box-sizing: border-box;
   position: relative;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  @media only screen and (max-width: 700px) {
+    display: block;
+  }
 }
 .show-color-picker {
-  position: absolute;
-  right: 20px;
   padding: 10px;
   font-size: 1.1rem;
   border: none;
   background: #c63b3b;
-  color: #d9d0d0;
+  color: #fff;
   cursor: pointer;
+  border-radius: 5px;
 }
 .color-picker {
-  position: absolute;
-  right: 20px;
-  margin-top: 45px;
-  z-index: 2;
+  width: 200px;
+  margin-top: -45px;
+  margin-left: 70px;
 }
 .sliders {
   margin-top: 50px;
 }
+.slider {
+  /* border: 1px solid red; */
+}
 .box-shadow {
   margin: 0 auto;
-  margin-top: calc(10vw - 10px);
   height: 300px;
   width: 80%;
   min-width: 300px;
   background-color: green;
-  border-radius: 20px;
+  border-radius: 5px;
   padding: 20px;
+  @media only screen and (max-width: 350px) {
+    margin-top: calc(10vw - 10px);
+  }
 }
 </style>
